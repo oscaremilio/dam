02a0410fb3e2121e4462c1ee69d9113773136e7e
oscaremilio <oscaremilio_@hotmail.es>
2021-02-04 17:22:41 +0100

    Ejercicio 1.01: "Hola Mundo"

---
+++ acceso-a-datos/Tema1_Ficheros/Ejercicio_1_01.java
@@ -0,0 +1,14 @@
+
+/*
+Instala el JDK, Eclipse y crea un programa escriba "Hola" en la pantalla.
+*/
+
+public class Ejercicio_1_01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola");
+
+    }
+    
+}
