df405e8596fcfef0b7b5d49e75436de621baabad
oscaremilio <oscaremilio_@hotmail.es>
2021-04-11 11:55:25 +0200

    Imprimir un número con el formato deseado

---
+++ programacion-de-servicios-y-procesos/Repaso-Java-Primero/Ejercicio_4_CircleArea.java
@@ -0,0 +1,30 @@
+
+/*
+Programa que define una constante de tipo float llamada PI
+con el valor de 3.14159.
+El programa pide al usuario el radio de un círculo,
+y visualizará el área (con 2 dígitos decimales) 
+del círculo (PI * radio * radio).
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio_4_CircleArea {
+
+    public static final float PI = 3.14159f;
+    public static void main(String[] args) {
+        
+        int radio;
+        float area;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el radio: ");
+        radio = sc.nextInt();
+        area = PI * radio * radio;
+
+        System.out.printf("El área del círculo es %.2f", area);
+
+        sc.close();
+    }
+}
